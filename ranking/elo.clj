60ef7665bb3ed474dbf59902c54f918d1e9de04d
Ibrahim Albarghouthi <ibrahimalbarghouthii@hotmail.com>
2017-11-20 14:18:18 +0200

    rating

--- ranking/elo.clj
+++ ranking/elo.clj
@@ -2,9 +2,37 @@
 
 (def logistic-base 400)
 
+(defn drop-nth [n coll]
+  (keep-indexed #(if (not= %1 n) %2) coll))
+
 (defn q-score [rating]
   (Math/pow 10 (/ rating logistic-base)))
 
-(defn expected-score [t1 t2]
-  (/ (apply + (map q-score t1))
-     (apply + (map q-score (concat t1 t2)))))
+(defn expected-score [players opponents]
+  (/ (apply + (map q-score players))
+     (apply + (map q-score (concat players opponents)))))
+
+(defn rating [players opponents state]
+  (+ (first players)
+     (* k-factor
+        (- state (expected-score players opponents))
+        (- 1 (/ (q-score (second players))
+                (apply + (map q-score players)))))))
+
+(defn new-rating [players results]
+  (map
+    (fn [ith]
+      (rating [(nth players ith)] (drop-nth ith players) (nth results ith)))
+    (range (count players))))
+
+
+(defn new-rating-partners [players results]
+  (map
+    (fn [ith]
+      (rating [(nth players ith)
+               (nth players (mod (+ 2 ith) (count players)))]
+              (drop-nth
+                (mod (+ 2 ith) (count players))
+                (drop-nth ith players)) (nth results ith)))
+    (range (count players))))
+
